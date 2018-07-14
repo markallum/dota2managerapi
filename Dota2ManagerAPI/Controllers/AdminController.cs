4f5ace2b891714e863d0f431c253d1a97d76aa63
Mark Allum <mark.allum@hotmail.co.uk>
2018-07-14 14:49:00 +0100

    Merged parser project into api project

---
+++ Dota2ManagerAPI/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dota2ManagerAPI.Web.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Admin")]
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        public async Task<string> RunParse()
+        {
+            //Grabber grabber = new Grabber();
+
+            return "done";
+        }
+    }
+}
---
+++ Dota2ManagerAPI/DAL/Parser/Grabber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Dota2StatisticsParser
+{
+    public class Grabber
+    {
+        private string _apiKey;
+
+        public Grabber()
+        {
+            _apiKey = File.ReadAllText("ValveAPIKey.txt");
+        }
+
+        public async Task<string> Grab()
+        {
+            return "";
+        }
+    }
+}
